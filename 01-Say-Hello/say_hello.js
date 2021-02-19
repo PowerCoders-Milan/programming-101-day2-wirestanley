c957ca99b496453267f1086f38ad92fd46710461
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-19 12:56:02 +0100

    exercise1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,20 @@
 //insert your pseudocode below
 /*
 
-here
+create a variable for name
+create a variable for greeting with a text 
+ask the user for his name
+put the name inside the variable name 
+print hello rawah
+print greeting + name 
+
+
 
 */
 
 //insert your code below
+var name2;
+
+
+name2 = window.prompt('please enter your name ');
+window.alert(' hello ' + String(name2));
