a9306ee1ed759d7f1ef50fc62faf8fdcdcb57a14
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-22 11:40:42 +0100

    exercise2,3

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
 /*
 
-here
-
+create a variable for string
+ask user for the string
+put the name of the string inside the variable
+display  the strings and lenght of the character
+print string + length of string + lenght of character
 */
 
 //insert your code below
+var name_of_car;
+
+
+name_of_car = window.prompt('please say the name of the car ');
+window.alert([name_of_car,'is',name_of_car.length,'character'].join(''));
--- 03-Find-My-Number/find_my_number.js
+++ 03-Find-My-Number/find_my_number.js
@@ -1,7 +1,18 @@
 //insert your pseudocode below
 /*
 
-here
+create a variable for 100
+is your number higher lower or equal 100
+is your number higher than 50
+is your number higher than 75
+loop repeat until it is solved
+loop repeat the question
+answer is equal, answer is higher, answer is lower.
+if answer is equal then it is solved
+if it is higher you have to find the next question
+if  it is lower you have to find the next question.
+
+
 
 */
 
